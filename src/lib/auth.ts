2411988cf6752d03d3b966d9305816704a0ba876
Luis Palacios <lrpalaciosdev@gmail.com>
2026-09-10 15:38:46 -0600

    add jwt path configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { drizzleAdapter } from '@better-auth/drizzle-adapter/relations-v2';
-import { type BetterAuthOptions, betterAuth, } from 'better-auth';
-import { jwt } from "better-auth/plugins";
+import { type BetterAuthOptions, betterAuth } from 'better-auth';
+import { jwt } from 'better-auth/plugins';
 import { admin } from 'better-auth/plugins/admin';
 import { db } from '../data/database.js';
 
@@ -11,9 +11,15 @@ const authConfig: BetterAuthOptions = {
 	emailAndPassword: {
 		enabled: true,
 	},
-	plugins: [admin(), jwt()],
+	plugins: [
+		admin(),
+		jwt({
+			disableSettingJwtHeader: true,
+			jwks: {
+				jwksPath: '/.well-known/jwks.json',
+			},
+		}),
+	],
 };
 
-export const auth = betterAuth(authConfig) as ReturnType<
-	typeof betterAuth<typeof authConfig>
->;
+export const auth = betterAuth(authConfig) as ReturnType<typeof betterAuth<typeof authConfig>>;
